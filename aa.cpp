2dec7aca3ddbdc786c79479cc91a220a05dbaac0
hidden482 <hiddengamerchinnu53659k@gmail.com>
2025-08-23 21:32:15 +0530

    V6

--- aa.cpp
+++ aa.cpp
@@ -1,8 +1,1 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-//stl=contaners+algo+iterators
-//containers  (object which stores data)--- 1.sequence containers  2.assocaitve containers  3.derived containers
-
-
-//version 4
+version 6
