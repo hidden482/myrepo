dfbe420fc74413c961137b73d8171e2f3bf1214a
hidden482 <hiddengamerchinnu53659k@gmail.com>
2025-08-23 19:40:34 +0530

    4rd version

--- aa.cpp
+++ aa.cpp
@@ -4,16 +4,5 @@ using namespace std;
 //stl=contaners+algo+iterators
 //containers  (object which stores data)--- 1.sequence containers  2.assocaitve containers  3.derived containers
 
-void display(vector<int> &a){
-    for (int i = 0; i < a.size(); i++)
-    {
-        cout<<a[i]<<endl;
-    }
-    
-}
-int main(){
-    int a;
-    
-}
 
-//version 2
+//version 4
